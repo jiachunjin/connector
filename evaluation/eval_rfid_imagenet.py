2de025e2bc81f6dcb4c6471da7653905ad8c5698
Jiachun Jin <jiachun22.jin@gmail.com>
2025-06-21 06:50:05 +0000

    sync

--- evaluation/eval_rfid_imagenet.py
+++ evaluation/eval_rfid_imagenet.py
@@ -3,11 +3,13 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import torch
+import torch_fidelity
 import torchvision.transforms as pth_transforms
 from omegaconf import OmegaConf
 from accelerate import Accelerator
 from util.dataloader import get_imagenet_wds_val_dataloader
 from evaluation.eval_rfid_imagenet_basic import AutoEncoder
+from tqdm import tqdm
 
 exp_dir = "/data/phd/jinjiachun/experiment/decoder/0619_decoder"
 config_path = os.path.join(exp_dir, "config.yaml")
@@ -26,8 +28,38 @@ autoencoder, dataloader = accelerator.prepare(autoencoder, dataloader)
 rank = accelerator.state.local_process_index
 world_size = accelerator.state.num_processes
 
+os.makedirs("evaluation/rec_img", exist_ok=True)
+os.makedirs("evaluation/ori_img", exist_ok=True)
+
 with torch.no_grad():
-    for batch in dataloader:
+    for i, batch in tqdm(enumerate(dataloader)):
         x = batch["pixel_values"]
+        x = x * 2 - 1
+        rec = autoencoder(x)
+
+        x = ((x + 1) / 2).clamp(0, 1)
+        rec = ((rec + 1) / 2).clamp(0, 1)
+
+        rec = pth_transforms.ToPILImage()(rec.cpu().squeeze(0))
+        ori = pth_transforms.ToPILImage()(x.cpu().squeeze(0))
+
+        rec.save(f"evaluation/rec_img/{rank}_{i}.png")
+        ori.save(f"evaluation/ori_img/{rank}_{i}.png")
+
         print(x.shape, x.device)
+        print(rec.shape, rec.device)
         break
+
+# accelerator.wait_for_everyone()
+# if accelerator.is_main_process:
+#     metrics_dict = torch_fidelity.calculate_metrics(
+#         input1  = "evaluation/ori_img",
+#         input2  = "evaluation/rec_img",
+#         cuda    = True,
+#         isc     = True,
+#         fid     = True,
+#         kid     = True,
+#         prc     = True,
+#         verbose = True,
+#     )
+#     print(metrics_dict)
--- evaluation/eval_rfid_imagenet.sh
+++ evaluation/eval_rfid_imagenet.sh
@@ -1,3 +1,3 @@
-accelerate launch --config_file \
+CUDA_VISIBLE_DEVICES=1,2 accelerate launch --config_file \
 config/parallel/default_config --main_process_port 30002 \
 evaluation/eval_rfid_imagenet.py
