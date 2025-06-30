2afd1f295211990089f9ced964311491f4d9ca8a
Jiachun Jin <jiachun22.jin@gmail.com>
2025-06-30 08:09:15 +0000

    sync

--- runner/test_dataloader.py
+++ runner/test_dataloader.py
@@ -49,6 +49,7 @@ def main():
                 print("跳过空批次")
                 continue
             num_samples += batch["pixel_values"].shape[0]
+            print(num_samples)
 
             if accelerator.sync_gradients:
                 global_step += 1
